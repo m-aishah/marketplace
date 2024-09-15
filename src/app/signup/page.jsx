2e5e1f8537e5b27b0313e0148a27b8132a32e0bc
Aishah <maishah2540@gmail.com>
2024-09-15 19:27:26 +0300

    Edit style of email field in signup page.

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -100,7 +100,7 @@ function Signup() {
                                 type="email"
                                 autoComplete="email"
                                 required
-                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-brand focus:border-brand focus:z-10 sm:text-sm"
+                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-brand focus:border-brand focus:z-10 sm:text-sm"
                                 placeholder="Email address"
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
