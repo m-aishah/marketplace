302b585450fb7008b10f30e8ce91b93da68de385
Dunamis <callmestiless@gmail.com>
2024-09-24 21:55:24 +0300

    feat: add create listing for apartment and skills

---
+++ src/app/(create-listings)/create-listing/apartments/types.js
@@ -0,0 +1,20 @@
+const types = [
+  {
+    id: 2,
+    name: "Studio",
+  },
+  {
+    id: 3,
+    name: "1 + 1 Apartment",
+  },
+  {
+    id: 4,
+    name: "2 + 1 Apartment",
+  },
+  {
+    id: 5,
+    name: "3 + 1 Apartment",
+  },
+];
+
+export default types;
---
+++ src/app/(create-listings)/create-listing/skills/categories.js
@@ -0,0 +1,20 @@
+const categories = [
+  {
+    id: 1,
+    name: "Technology & Development",
+  },
+  {
+    id: 2,
+    name: "Design & Creative",
+  },
+  {
+    id: 3,
+    name: "Education & Training",
+  },
+  {
+    id: 4,
+    name: "Other",
+  },
+];
+
+export default categories;
