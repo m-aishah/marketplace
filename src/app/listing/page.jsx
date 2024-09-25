153fb984c82a36772038f5399ba9177fb91a8a50
Aishah <maishah2540@gmail.com>
2024-09-25 22:01:48 +0300

    Make listing page protected route?

--- src/app/listing/page.jsx
+++ src/app/listing/page.jsx
@@ -5,7 +5,7 @@ import ApartmentListingPage from "./listingCategory/apartmentListing";
 import GoodsListingPage from "./listingCategory/goodsListing";
 import FreelancerServicePage from "./listingCategory/serviceListing";
 
-
+// TODO: Do we want the lisitng page to only be accessible to logged in user? or it is if user tries add to cart or whatever that the user should be promptd to login?
 const listingPage = () => {
     return (
         <ProtectedRoute>
