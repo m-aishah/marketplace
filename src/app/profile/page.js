2e6f35e3a42bb8759d8570db1008cc89cb64f5c2
abdomody35 <abdomody35@gmail.com>
2024-10-15 13:38:48 +0300

    fix profile: listings active state;

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -15,7 +15,7 @@ import LoadingSpinner from "@/components/LoadingSpinner";
 function Profile() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [activeTab, setActiveTab] = useState("contact");
+  const [activeTab, setActiveTab] = useState("listings");
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -81,7 +81,7 @@ function Profile() {
             <nav className="-mb-px flex">
               <button
                 className={`mr-8 py-4 px-1 border-b-2 font-medium text-sm ${
-                  activeTab === "lisitngs"
+                  activeTab === "listings"
                     ? "border-blue-500 text-blue-600"
                     : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
                 }`}
