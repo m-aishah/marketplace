9cf148d9ebf12b8fec43fa3a92b8abb53a3a13fb
Aishah <maishah2540@gmail.com>
2024-10-15 11:17:37 +0300

    Change arrangement of tab fields.

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -81,17 +81,7 @@ function Profile() {
             <nav className="-mb-px flex">
               <button
                 className={`mr-8 py-4 px-1 border-b-2 font-medium text-sm ${
-                  activeTab === "contact"
-                    ? "border-blue-500 text-blue-600"
-                    : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
-                }`}
-                onClick={() => setActiveTab("contact")}
-              >
-                Contact Information
-              </button>
-              <button
-                className={`mr-8 py-4 px-1 border-b-2 font-medium text-sm ${
-                  activeTab === "listings"
+                  activeTab === "lisitngs"
                     ? "border-blue-500 text-blue-600"
                     : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
                 }`}
@@ -100,14 +90,24 @@ function Profile() {
                 Listings
               </button>
               <button
-                className={`py-4 px-1 border-b-2 font-medium text-sm ${
+                className={`mr-8 py-4 px-1 border-b-2 font-medium text-sm ${
                   activeTab === "transactions"
                     ? "border-blue-500 text-blue-600"
                     : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
                 }`}
                 onClick={() => setActiveTab("transactions")}
               >
-                Transaction History
+                Transactions
+              </button>
+              <button
+                className={`py-4 px-1 border-b-2 font-medium text-sm ${
+                  activeTab === "contact"
+                    ? "border-blue-500 text-blue-600"
+                    : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+                }`}
+                onClick={() => setActiveTab("contact")}
+              >
+                Contact Details
               </button>
             </nav>
           </div>
